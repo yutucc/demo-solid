6828542fa5310bc2ad1f402d99c71c1f14ac42ee
wuqinfa <369724377@qq.com>
2022-09-16 07:18:37 +0800

    chore: 完成积木的匹配

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import matcher from '@/matcher';
 import Rope from '@/utils/rope';
 
 
-export default (referBlocks: Blocks, answerBlocks: Blocks) => {
+const blockLint = (referBlocks: Blocks, answerBlocks: Blocks) => {
   const referRope = new Rope(referBlocks, '*sB)HodUVFs[3fs_m^e?');
   const answerRope = new Rope(answerBlocks, 'nj9Unbj#GRR]!mL%)}:e');
 
@@ -20,6 +20,23 @@ export default (referBlocks: Blocks, answerBlocks: Blocks) => {
   const referBlockIds = referRope.getBlockIds();
   const answerConnections = answerRope.getConnections();
   const answerBlockIds = answerRope.getBlockIds();
+  let isAllMatch = true;
 
+  let refConnectionsIndex = 0;
+  for (const refBlock of referConnections) {
+    if (!matcher(refBlock, answerConnections[refConnectionsIndex], referBlockIds, answerBlockIds)) {
+      isAllMatch = false;
+      break;
+    }
 
+    refConnectionsIndex += 1;
+  }
+
+  if (isAllMatch) {
+    console.log('两条积木完全相同');
+  } else {
+    console.log('两条积木不相同');
+  }
 };
+
+blockLint(data.refer, data.target);
